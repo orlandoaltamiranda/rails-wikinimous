835919d42caa4d14df1c0790b5142ded3815b913
Orlando Altamiranda <orlandoaltamiranda@hotmail.com>
2021-08-12 14:41:49 -0400

    Add controller articles and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#   Create 10 fakes articles 
+10.times do
+  new_article = Article.create(title: Faker::Book.title, content: Faker::Quote.yoda)
+end
